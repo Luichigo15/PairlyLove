83e1c4117e6c4a75fac1fa88071b07e4b720de4d
Luichigo15 <grovile11@hotmail.com>
2026-02-02 21:07:07 -0600

    chore: Role title animation added

--- app/src/main/java/app/luichigo15/pairly/ui/role/PLRoleScreen.kt
+++ app/src/main/java/app/luichigo15/pairly/ui/role/PLRoleScreen.kt
@@ -1,28 +1,23 @@
 package app.luichigo15.pairly.ui.role
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandHorizontally
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkHorizontally
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import app.luichigo15.pairly.R
-import app.luichigo15.pairly.ui.role.widget.PLRoleCard
 import app.luichigo15.pairly.ui.role.widget.PLRoleSelected
 import app.luichigo15.pairly.ui.role.widget.PLRoleSelection
 import app.luichigo15.pairly.ui.theme.PLTheme
@@ -30,6 +25,10 @@ import app.luichigo15.pairly.ui.theme.PLTheme
 @Composable
 fun PLRoleScreen(modifier: Modifier = Modifier) {
     var showSelection by remember { mutableStateOf(false) }
+    val titlePadding by animateDpAsState(
+        targetValue = if (showSelection) 10.dp else 100.dp,
+        animationSpec = tween(1000)
+    )
 
     ConstraintLayout(
         modifier = modifier
@@ -39,6 +38,14 @@ fun PLRoleScreen(modifier: Modifier = Modifier) {
         val (rolesRef, titleRef, selectedRef) = createRefs()
         val guideLine = createGuidelineFromTop(0.5f)
 
+        Text(
+            stringResource(R.string.pl_role_title), modifier = Modifier
+                .constrainAs(titleRef) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }.padding(top = titlePadding),
+            style = MaterialTheme.typography.headlineLarge)
         PLRoleSelection(
             isVisible = !showSelection,
             modifier = Modifier.constrainAs(rolesRef) {
@@ -50,7 +57,6 @@ fun PLRoleScreen(modifier: Modifier = Modifier) {
             onRoleSelected = {
                 showSelection = true
             })
-
         PLRoleSelected(
             isVisible = showSelection,
             modifier = Modifier.constrainAs(selectedRef) {
@@ -63,7 +69,6 @@ fun PLRoleScreen(modifier: Modifier = Modifier) {
             }
         )
     }
-
 }
 
 @Preview(showBackground = true)
